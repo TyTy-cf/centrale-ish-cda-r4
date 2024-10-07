2976fc27fee2e2dc718f14ff792e55b5c6dbea4f
Kevin Tourret <kevin@drosalys.fr>
2024-10-07 16:28:50 +0200

    add a better json test

--- src/test/java/fr/cda/centaleish/controller_rest/SecurityRestControllerTest.java
+++ src/test/java/fr/cda/centaleish/controller_rest/SecurityRestControllerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.hasSize;
 
 import fr.cda.centaleish.dto.user.UserCreateDTO;
 import org.json.JSONObject;
@@ -71,22 +72,20 @@ public class SecurityRestControllerTest {
                 ))));
 
         resultActions.andExpect(status().is2xxSuccessful())
-                .andExpect(jsonPath("$.uuid").exists())
-                .andExpect(jsonPath("$.email").exists())
-                .andExpect(jsonPath("$.firstName").exists())
-                .andExpect(jsonPath("$.lastName").exists())
-                .andExpect(jsonPath("$.phone").exists())
-                .andExpect(jsonPath("$.siret").isEmpty())
-                .andExpect(jsonPath("$.photo").isEmpty())
-                .andExpect(jsonPath("$.birthAt").exists())
-                .andExpect(jsonPath("$.createdAt").exists())
-                .andExpect(jsonPath("$.favorites").exists())
-                .andExpect(jsonPath("$.isAdmin").value(Boolean.FALSE))
-                .andExpect(jsonPath("$.isActive").value(Boolean.FALSE))
-                .andExpect(jsonPath("$.password").doesNotExist())
-                .andExpect(jsonPath("$.roles").doesNotExist())
-                .andExpect(jsonPath("$.activationCode").doesNotExist())
-                .andExpect(jsonPath("$.activationCodeSentAt").doesNotExist());
+            .andExpectAll(
+                jsonPath("$.uuid").exists(),
+                jsonPath("$.email").exists(),
+                jsonPath("$.firstName").exists(),
+                jsonPath("$.lastName").exists(),
+                jsonPath("$.phone").exists(),
+                jsonPath("$.siret").isEmpty(),
+                jsonPath("$.photo").isEmpty(),
+                jsonPath("$.birthAt").exists(),
+                jsonPath("$.createdAt").exists(),
+                jsonPath("$.favorites").exists(),
+                jsonPath("$.isAdmin").value(Boolean.FALSE),
+                jsonPath("$.isActive").value(Boolean.FALSE),
+                jsonPath("$.*", hasSize(12)));
 }
 
     @Test
